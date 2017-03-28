3e68dd161f597d2390e1029f3c7ac15741ffafd4
Ruby Chen <ruby@yun-ru.com>
2017-03-29 07:24:08 +0800

    home page qrcode modal

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,92 @@
 <template>
-  <v-app top-toolbar left-fixed-sidebar>
-    <header>
-      <v-toolbar class="trans-dark">
-        <v-toolbar-side-icon class="hidden-md-and-up" @click.native.stop="sidebar2 = !sidebar2" />
-        <v-toolbar-logo>LOGO</v-toolbar-logo>
-        <v-spacer />
-        <v-btn icon dark>
-          <v-icon>account_circle</v-icon>
-        </v-btn>
-        <v-btn icon dark>
-          <v-icon>favorite</v-icon>
-        </v-btn>
-      </v-toolbar>
-    </header>
+  <div id="app">
+    <transition name="fade">
+      <div v-if="isShowQRCodeModal" id="qrcode-modal" class="my-modal-wrap" @click.self="isShowQRCodeModal = false">
+        <div class="modal-box">
+          <div class="close-btn"  @click="isShowQRCodeModal = false">
+            <v-icon>clear</v-icon>
+          </div>
+          <div class="modal-box-content">
+            <div style="margin-top: 10px"></div>
+            <h4>下載官方APP<br>預約更即時！</h4>
+            <div><img src="/static/imgs/qrcode.png"></div>
+            <div style="margin-top: 5px"></div>
+            <div><img src="/static/imgs/appstore.png"></div>
+            <div style="margin-top: 5px"></div>
+            <div><img src="/static/imgs/googleplay.png"></div>
+          </div>
+        </div>
+      </div>
+    </transition>
 
-    <main>
-      <v-sidebar fixed v-model="sidebar2">
-        <div class="logo">LOGO</div>
-        <v-list>
-          <v-list-item v-for="item in menu">
-            <v-list-tile ripple v-if="item.title">
-              <v-list-tile-title v-text="item.title" />
-            </v-list-tile>
-            <v-divider v-else-if="item.divider" light />
-          </v-list-item>
-        </v-list>
-      </v-sidebar>
+    <v-app top-toolbar left-fixed-sidebar>
+      <header>
+        <v-toolbar class="trans-dark">
+          <v-toolbar-side-icon class="hidden-md-and-up" @click.native.stop="sidebar2 = !sidebar2" />
+          <v-toolbar-logo><img src="/static/imgs/logo.png"></v-toolbar-logo>
+          <v-spacer />
+          <v-btn icon dark>
+            <v-icon>account_circle</v-icon>
+          </v-btn>
+          <v-btn icon dark>
+            <v-icon>favorite</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </header>
+      <main>
+        <v-sidebar fixed v-model="sidebar2">
+          <div class="logo"></div>
+          <v-list>
+            <v-list-item>
+              <v-list-tile ripple>
+                <v-list-tile-title>餐點瀏覽</v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+            <v-list-item>
+              <v-list-tile ripple>
+                <v-list-tile-title>訂位點餐</v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+            <v-list-item>
+              <v-list-tile ripple>
+                <v-list-tile-title>動態消息</v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+            <v-list-item>
+              <v-list-tile ripple>
+                <v-list-tile-title>商店資訊</v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+            <v-list-item>
+              <v-list-tile ripple>
+                <v-list-tile-title>會員中心</v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+            <v-list-item>
+              <v-list-tile ripple>
+                <v-list-tile-title @click="isShowQRCodeModal = true">APP下載</v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+            <v-list-item>
+              <v-divider light />
+            </v-list-item>
+            <v-list-item>
+              <v-list-tile ripple>
+                <v-list-tile-title>會員登入</v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+          </v-list>
+        </v-sidebar>
 
-      <router-view></router-view>
 
-    </main>
-  </v-app>
+        <router-view></router-view>
+
+      </main>
+
+
+    </v-app>
+
+  </div>
 </template>
 
 <script>
@@ -39,17 +95,10 @@ export default {
   data() {
     return {
       sidebar2: false,
-      menu: [
-        { title: '餐點瀏覽' },
-        { title: '訂位點餐' },
-        { title: '動態消息' },
-        { title: '商店資訊' },
-        { title: '會員中心' },
-        { title: 'APP下載' },
-        { divider: true },
-        { title: '會員登入' },
-      ]
+      isShowQRCodeModal: false,
     }
+  },
+  methods: {
   }
 }
 </script>
